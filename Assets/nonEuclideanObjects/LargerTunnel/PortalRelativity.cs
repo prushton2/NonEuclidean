97de023947dfb0f31ef94b99d30b71b8a86ee347
prushton2 <peterrushton52@gmail.com>
2021-06-06 10:11:16 -0400

    Added old assets

---
+++ Assets/nonEuclideanObjects/LargerTunnel/PortalRelativity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalRelativity : MonoBehaviour
+{
+
+    //allows an object to stay relative to another object with a set distance
+
+    // center of the portal that will display what the attached camera is watching
+    public GameObject inputPortal;
+    // center of the the other portal
+    public GameObject outputPortal;
+    // the object defining the an, the player's camera in most cases
+    public GameObject relativeObject;
+
+    //holds the distance between the relativeObject and the inputPortal
+    private Vector3 deltaPos;
+    public Vector3 deltaAngle;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        deltaPos = inputPortal.transform.position - relativeObject.transform.position;
+        this.transform.position = outputPortal.transform.position - deltaPos;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(inputPortal.transform.rotation, outputPortal.transform.rotation);
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * relativeObject.transform.forward;
+        this.transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+
+    }
+}
